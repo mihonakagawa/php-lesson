f1b2c54b09bdeb95d3f20b3cb5e302ccf5582bb9
中川 <nakagawa@gizumo-admin-28noMacBook-Air.local>
2025-02-12 13:27:48 +0900

    add: コメント

--- test_app/new.php
+++ test_app/new.php
@@ -7,6 +7,7 @@
 <body>
   <form action="store.php" method="post">
     <!-- 新規作成② formタグのaction属性に指定したファイルに対してpostの通信を行う(データベースに対して直接POSTしている訳ではない) -->
+     <!-- Q.なんでe関数はnew.phpに実装がない？　→　表示時にスクリプトが実行されるから -->
     <input type="text" name="content">
     <input type="submit" value="作成">
   </form>
